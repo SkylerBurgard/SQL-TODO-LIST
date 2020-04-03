7d28b223df85876f014054874a32ab0ebf0f533f
Skyler Burgard <skylerrburgard@hotmail.com>
2020-04-03 15:47:20 -0500

    Initial commit and creates basic static file server

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser'); //these are our import statements
+const PORT = process.env.PORT || 5000; //sets port
+
+app.use(express.static('server/public')); //serves static files
+app.use(bodyParser.urlencoded({ extended: true })); //allows us to utilize our front end.
+app.use(bodyParser.json()); //This allows us to use post man.
+
+app.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}`);
+
+
+});
